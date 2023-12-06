c9636f8d834cdc23cee33cbbd8e5fd2aa11819e4
Pedro Henrique Balbino Rocha <pedrorochabrasil@gmail.com>
2023-12-06 17:09:30 -0300

    Update Main.py
    Função para adicionar e categorizar funcionando

--- Main.py
+++ Main.py
@@ -1,49 +1,61 @@
 import pandas as pd
 
 def add_item(df, product, type, model, color, size, quantity):
-
-    print("""Please, enter in this sequence:""")
-
-    new_row = input('Product:'), input('Type:'), input('Model:'), input('Color:'), input('Size:'), input('Quantity:')
-
+    
     new_row = pd.DataFrame({'Produto': [product], 'Tipo': [type], 'Modelo': [model], 'Cor': [color], 'Tamanho': [size], 'Quantidade': [quantity]})
 
-    df = add_item(df, 'Macarrão', 'Não perecível', 'Penny', 'Qualquer', '100kg',200)
+    return pd.concat([df, new_row], ignore_index=True)
 
-data = {'Produto' : ['Tira', 'Forma', 'Borracha', 'Símbolo','Cola','Couro'], 
-        'Tipo': ['Grossa','Fina','Infantil','Regular','100', '90/10','70/30','Preto','Branco','Para Couro','Texturizado'],
-        'Modelo': ['Aviador','Regular', 'Brilhante','Lisa','Texturizada','Padrão','Logotipo','TekBond','Courino'],
-        'Cor': ['Branco', 'Preto','Azul','Vermelho','Rosa','Verde','Laranja','Ciano','Roxo','Prata','Dourado'],
-        'Tamanho': ['20/21','22/23','24/25','26/27','28/29','30/31','32/33','34/35','36/37','38/39','40/41', '90cm','--','120cm','20g','Normal'],
-        'Quantidade': [10]
+data = {'Produto' : ['Tira', 'Forma', 'Borracha'], 
+        'Tipo': ['Lisa', 'Metálica', '90/10'],
+        'Modelo': ['Aviador','Regular', 'Comum'],
+        'Cor': ['Branca', '--',  'Azul/Vermelho'],
+        'Tamanho': ['22', '22', '90cm'],
+        'Quantidade': [10, 1, 2]
 }
 
 df = pd.DataFrame(data)
 
-#df.loc[df['Cor'] ==   'Qualquer', 'Quantidade'] = df.loc[df['Cor'] ==   'Qualquer', 'Quantidade'] - 5
-
 df.to_csv('teste.csv', index=False)
 
-
 while True:
+    print("""
+    Welcome!
 
-  print("""Welcome!
-
-  Please, select you option:
+    Please, select your option:
 
     1 - Add
     2 - Reorder Alerts
     3 - Supplier Management
     0 - exit
+    """)
 
-  """)
+    action = input('')
 
-  action = input('')
+    if action == '1':
+        # Novo Item!
 
-  if action == '1':
+        print("Please enter in this sequence:")
 
-    add_item('','','','','','','')
+        product = input('Product: ')
+        type = input('Type: ')
+        model = input('Model: ')
+        color = input('Color: ')
+        size = input('Size: ')
+        quantity = input('Quantity: ')
 
-  elif action == '0':
+        # Se não tiver quantidade vai ficar sendo zero
+        quantity = 0 if not quantity else quantity
 
-    quit()
+        # Chama a função para adicionar os itens
+        df = add_item(df, product, type, model, color, size, quantity)
+
+        #print(df) # Debug
+
+        df.to_csv('teste.csv', index=False) 
+
+        print("Item added successfully!")
+
+
+    elif action == '0':
+        break
