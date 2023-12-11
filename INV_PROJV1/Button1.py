66afb531dda510f0f0a66a564fda4f3c3352d224
Pedro Henrique Balbino Rocha <pedrorochabrasil@gmail.com>
2023-12-11 01:41:26 -0300

    Atualização de Funções
    Programa modularizado e funções revisadas. Bug de arquivo duplicado corrigido e função de gerenciamento de fornecedores implementada.

---
+++ INV_PROJV1/Button1.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import Button3
+
+def print_df(df):
+    print(df)
+
+def validar(df, product):
+    if product in df['Produto'].values:
+        print(f"O produto {product} já está catalogado\n")
+        return True
+
+def add_item(df, product, type, model, color, size, quantity):
+    if(validar(df, product)):
+        return df
+    
+    new_row = pd.DataFrame({'Produto': [product], 'Tipo': [type], 'Modelo': [model], 'Cor': [color], 
+                            'Tamanho': [size], 'Quantidade': [quantity]})
+    return pd.concat([df, new_row], ignore_index=True)
+
+def add_item_and_update_s(p_df, product, type, model, color, size, quantity):
+
+    p_df = add_item(p_df, product, type, model, color, size, quantity)
+
+    return p_df
+
+def opt1():
+# Novo Item!
+
+        print("Please enter in this sequence atributes product:")
+
+        product = input('Product: ')
+        type = input('Type: ')
+        model = input('Model: ')
+        color = input('Color: ')
+        size = input('Size: ')
+        quantity = input('Quantity: ')
+
+        # Se não tiver quantidade vai ficar sendo zero
+        quantity = 0 if not quantity else quantity
+
+        # Chama a função para adicionar os itens
+        p_df = add_item_and_update_s(p_df, product, type, model, color, size, quantity)
+
+        print("Item added successfully!")
+        print_df(p_df)
+        print("\n")
+
+        p_df.to_csv("Products.csv", index=False)
+
+        print('Actual suppliers:')
+        print("\n")
+        print_df(s_df)
+        print("\n")
+
+        sup = input('Would you like to add a new supplier to this item? Y/N: ')
+
+        if sup == 'Y':
+            
+            supplier = input('Supplier: ')
+            contact = ''
+            email = ''
+            CEP = ''
+            s_df =Button3.manage_s(s_df, supplier, product, contact, email, CEP)
+            s_df.to_csv('Suppliers.csv', index=False)
+
+            print('New Supplier! To update the informations select the option 3.')
+            print('\n')
+            print(s_df)
+            print('\n')
+
+        elif sup == 'N':
+
+            print('Ok!')
+            print('\n')
+
+
---
+++ INV_PROJV1/Button3.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import Button1
+
+def update_item(s_df, index, column, new_value):
+
+  s_df.at[index, column] = new_value
+  return s_df
+
+def add_s(df, supplier, product, contact, email, CEP):
+        new_roll = pd.DataFrame({'Supplier':[supplier], 'Produto': [product], 'Contato': [contact], 'E-Mail': [email], 'CEP': [CEP]})
+        return pd.concat([df, new_roll], ignore_index=True)
+
+def listar_s(df, product):
+    if product in df['Produto'].values:
+        print(f"Fornecedor de : {df['Produto'].values}")
+
+def opt3():
+     
+    print("""
+          
+    Welcome to Supplier Management!
+           
+    What do you want to do?
+           
+        1 - Add
+        2 - Edit
+        3 - Remove
+        0 - Back
+          
+    """)
+    
+    nome = 'Suppliers.csv'
+
+    s_df = pd.read_csv(nome)
+
+    S = input('Enter the option: ')
+
+    if S == '1':
+
+        #nome = 'Suppliers.csv'
+
+        #s_df = pd.read_csv(nome)
+
+        print('\n')
+        print(s_df)
+        print('\n')
+
+        supplier = input('Supplier: ')
+        product = input('Product: ')
+        contact =input('Contact: ')
+        email = input('E-mail: ')
+        CEP = input('CEP: ')
+
+        s_df = add_s(s_df, supplier, product, contact, email, CEP)
+
+        s_df.to_csv("Suppliers.csv", index = False)
+
+        print('\n')
+        print('New Supplier added:')
+        print('\n')
+        print(s_df)
+
+    elif S == '2':
+        
+        #nome = 'Suppliers.csv'
+
+        #s_df = pd.read_csv(nome)
+
+        print("Choose a column to update(Enter the class you want to edit):")
+        print(s_df.columns)
+
+        column = input('')
+        print("\nAvailable items:")
+        print(s_df.to_string(index=True))
+
+        try:
+            index = int(input('\nEnter the index of the item to update: '))
+            if 0 <= index < len(s_df):
+                new_value = input(f'Enter the new value for {column}: ')
+                s_df = update_item(s_df, index, column, new_value)
+                s_df.to_csv('Suppliers.csv', index=False)
+                print("Item updated successfully!")
+            else:
+                print("Invalid index. Please enter a valid index.")
+        except ValueError:
+                print("Invalid input. Please enter a valid index.")
+
+    elif S == '3':    
+        
+        print("Choose a column to delete(Enter the class you want to edit):")
+        print(s_df.columns)
+
+        column = input('')
+        print("\nAvailable Suppliers:")
+        print(s_df.to_string(index=True))
+
+        try:
+            index = int(input('\nEnter the index of the supplier to delete: '))
+            if 0 <= index < len(s_df):
+
+                s_df = s_df.drop(index, axis = 0)
+                s_df.to_csv('Suppliers.csv', index=False)
+                print('\n')
+                print(s_df)
+                print('\n')
+                print("Item updated successfully!")
+            else:
+                print("Invalid index. Please enter a valid index.")
+        except ValueError:
+                print("Invalid input. Please enter a valid index.")
+
+    elif S == '0':
+         
+        print('OK!')
