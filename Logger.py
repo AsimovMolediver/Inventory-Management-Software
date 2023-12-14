aae48b4c561b6390f2256d12df4ed99f105cdd9c
Pedro Henrique <pedrorochabrasil@gmail.com>
2023-12-13 23:11:45 -0300

    Certo

--- Logger.py
+++ Logger.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import pandas as pd
 
 def registrar_log(funcao, linha_modificada):
     data_hora_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -15,3 +16,15 @@ def registrar_log7(**linha_modificada):
 
     registrar_log('VENDA',linha_modificada)
     print("\n")
+
+
+
+def imprimir_csv():
+    nome = 'log.csv'
+    try:
+        df = pd.read_csv(nome, encoding='utf-8')
+    except UnicodeDecodeError:
+        # Se ocorrer um erro, tente outro encoding
+        df = pd.read_csv(nome, encoding='latin-1')
+    
+    print(df)
